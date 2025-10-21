65f8ec7c5379d02918ef19d1a7bc2f7bda70803f
STUDENT <STUDENT@DESKTOP-TC01TCH>
2025-10-21 16:50:13 +0400

    Add Task1

---
+++ Task1_VazhaTsamalashvili/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1_VazhaTsamalashvili
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Vazha");
+        }
+    }
+}
