745c031f6e0e54bf0c7996e19b6d7ff02f5e48d1
Student <wamala731@gmail.com>
2025-10-28 14:53:21 +0400

    Add Task2

---
+++ Task2_VazhaTsamalashvili/Task2_VazhaTsamalashvili/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task2_VazhaTsamalashvili
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short myShort = 1;
+            int myInt = 2;
+            long myLong = 3;
+            ushort myUshort = 4;
+            uint myUint = 5;
+            ulong myUlong = 6;
+            byte myByte = 7;
+            sbyte mySbyte = 8;
+            float myFloat = 3.14f;
+            double myDouble = 3.1415926535;
+            decimal myDecimal = 147189471;
+            char myChar = 'A';
+            string myString = "Task2";
+            bool myBool = true;
+            
+            Console.WriteLine(myShort);
+            Console.WriteLine(myInt);
+            Console.WriteLine(myLong);
+            Console.WriteLine(myUshort);
+            Console.WriteLine(myUint);
+            Console.WriteLine(myUlong);
+            Console.WriteLine(myByte);
+            Console.WriteLine(mySbyte);
+            Console.WriteLine(myFloat);
+            Console.WriteLine(myDouble);
+            Console.WriteLine(myDecimal);
+            Console.WriteLine(myChar);
+            Console.WriteLine(myString);
+            Console.WriteLine(myBool);
+        }
+    }
+}
