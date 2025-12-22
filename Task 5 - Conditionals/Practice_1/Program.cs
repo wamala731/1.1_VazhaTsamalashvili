e09046ee7f3c8e4e3707023642c44b006204d188
Student <wamala731@gmail.com>
2025-12-22 23:19:55 +0400

    Add Task 5

---
+++ Task 5 - Conditionals/Practice_1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Practice_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first Number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second Number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter third Number: ");
+            int num3 = int.Parse(Console.ReadLine());
+            bool isNegative = false;
+            if (num1 < 0)
+            {
+                isNegative = true;
+            }
+            else if (num2 < 0)
+            {
+                isNegative = true;
+            }
+            else if (num3 < 0)
+            {
+                isNegative = true;
+            }
+            int max = num1;
+            if (max < num2)
+            {
+                max = num2;
+            }
+            else if (max < num3)
+            {
+                max = num3;
+            }
+
+            Console.WriteLine("The greatest number: " + max);
+            if (isNegative) {
+                Console.WriteLine("Negative number contained");
+            }
+        }
+    }
+}
---
+++ Task 5 - Conditionals/Practice_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Specialized;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Practice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======= Menu =======");
+            Console.WriteLine("1. The current month");
+            Console.WriteLine("2. The current day of week");
+            Console.WriteLine("3. The current year");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("===================");
+            Console.WriteLine("Select Menu number:");
+            DateTime dateNow = DateTime.Now;
+            while (true)
+            {
+                Console.Write("Enter your choise: ");
+                if (int.TryParse(Console.ReadLine(), out int UserChoise))
+                {
+                    if (UserChoise == 0)
+                    {
+                        return;
+                    }
+                    else if (UserChoise == 1)
+                    {
+                        Console.WriteLine(dateNow);
+                        Console.WriteLine("Current month: " + dateNow.Month);
+                    }
+                    else if (UserChoise == 2)
+                    {
+                        Console.WriteLine(dateNow);
+                        Console.WriteLine("Current day of week: " + dateNow.DayOfWeek);
+                    }
+                    else if (UserChoise == 3)
+                    {
+                        Console.WriteLine(dateNow);
+                        Console.WriteLine("Current year: " + dateNow.Year);
+                    }
+
+                    else if (UserChoise > 3)
+                    {
+                        Console.WriteLine("You must choose one of the following numbers: ");
+                    }
+                }
+
+
+            }
+        }
+    }
+}
