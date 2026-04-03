ed1565a902327214e25a98d700490f83b14b32d4
Wamala <wamala731@gmail.com>
2026-04-03 16:32:48 +0400

    Pracitce 3

---
+++ Practice_3/Practice_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Practice_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists(@"..\..\..\MyDirInfo"))
+            {
+                Directory.CreateDirectory(@"..\..\..\MyDirInfo");
+            }
+           
+            string info = "Vazha Tsamalashvili \n Proffesion is Student \n Contact: \n Levan Kbilashvili Highway N16\n 557-56-48-12 \n wamala731@gmail.com \n https://github.com/wamala731/1.1_VazhaTsamalashvili.git";
+            File.WriteAllText(@"..\..\..\MyDirInfo\Vazha_Tsamalashvil_04.03.2026.txt", info);
+
+        }
+    }
+}
